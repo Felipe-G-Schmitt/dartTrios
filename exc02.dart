9a723904620c9b0cc82c1986bbfb69c61955247e
felipe <felipe.hlvieira@gmail.com>
2026-03-12 20:00:46 -0300

    exc02 and exc03 finished

--- exc02.dart
+++ exc02.dart
@@ -1,36 +1,36 @@
-// Necessário executar este código no dartpad pois no vscode isto é so um diretório!!!
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+// Necessário executar este código no dartpad pois no vscode isto é so um diretório!!!  Retire tudo abaixo da linha 1 dos comentários com 'ctrl + :'
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;     
 
-Future<Map<String, dynamic>> buscarClima() async {
-  const lat = -26.3043758;
-  const lon = -48.8463744;
+// Future<Map<String, dynamic>> buscarClima() async {
+//   const lat = -26.3043758;
+//   const lon = -48.8463744;
   
-  final url = Uri.parse(
-    'https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$lon&current_weather=true'
-  );
+//   final url = Uri.parse(
+//     'https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$lon&current_weather=true'
+//   );
 
-  try {
-    final resposta = await http.get(url);
+//   try {
+//     final resposta = await http.get(url);
 
-    if (resposta.statusCode == 200) {
-      final dados = jsonDecode(resposta.body);
-      return dados['current_weather'];
-    } else {
-      throw Exception('Erro na requisição');
-    }
-  } catch (e) {
-    throw Exception('Falha ao conectar: $e');
-  }
-}
+//     if (resposta.statusCode == 200) {
+//       final dados = jsonDecode(resposta.body);
+//       return dados['current_weather'];
+//     } else {
+//       throw Exception('Erro na requisição');
+//     }
+//   } catch (e) {
+//     throw Exception('Falha ao conectar: $e');
+//   }
+// }
 
-void main() async {
-  print('Buscando clima!');
-  try {
-    final clima = await buscarClima();
-    print('Temperatura atual: ${clima['temperature']}°C');
-    print('Velocidade do vento: ${clima['windspeed']} km/h');
-  } catch (e) {
-    print('Erro: $e');
-  }
-}
+// void main() async {
+//   print('Buscando clima!');
+//   try {
+//     final clima = await buscarClima();
+//     print('Temperatura atual: ${clima['temperature']}°C');
+//     print('Velocidade do vento: ${clima['windspeed']} km/h');
+//   } catch (e) {
+//     print('Erro: $e');
+//   }
+// }
